d46294fc2d53c2c62b7ceba28c666bdf13eb4ceb
Meraj786 <ansarimeraj120@gmail.com>
2025-10-06 20:00:27 +0530

    My 1st test

--- src/FirstQuestion.java
+++ src/FirstQuestion.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+package tests;
+
+import org.testng.annotations.Test;
 
 public class FirstQuestion {
+
+    @Test
+    public void Sum() {
+        System.out.println("Added numbers.");
+        int x = 100;
+        int y = 200;
+        int x+y = 300;
+        System.out.println("Do addition");
+        int z = x + y;
+        System.out.println(z);
+        System.out.println("Check result");
+
+        System.out.println("Pass");
+    }
 }
